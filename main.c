d2cfa47cb870ec4f6cbe5d284781fa174e304fb4
Renan Moreira <renanmoreira17@gmail.com>
2022-03-11 13:44:25 -0300

    implementando fucnao de divisao

--- main.c
+++ main.c
@@ -10,10 +10,16 @@ int mult(int a, int b)
     return a*b;
 }
 
+float div(int a, int b)
+{
+    return (float)a/b;
+}
+
 int main()
 {
     int a, b;
     printf("Hello World!\n");
     scanf("%d %d", &a, &b);
+    float c = div(a, b);
     return 0;
 }
